c43cc9211e9994ee0055011fceeee662b50c5432
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2023-05-22 22:59:26 -0300

    adding files

---
+++ README.md
@@ -0,0 +1,8 @@
+# evolucao_basquete
+
+## Description
+I play basketball occasionally and I wanted to maintain a record of how much do I play, what grade would I give for the game and how much had I played.
+For this, I am currently using notion, and it is a good tool but I wanted to gather its information to display on a dashboard, where I could keep track of all the info I wanted.
+This project comes in handy as it helps me connecting the notion database that I update everytime I play.
+
+![Semantic description of image](/img_db_basquete.png "img_db_basquete")*img_db_basquete*]
---
+++ evolucao_basquete.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+import json
+
+def get_dados_notion():
+    token = $NOTION_BASQUETE_DB_ID
+    database_id = $NOTION_BASQUETE_TOKEN
+
+    url = f'https://api.notion.com/v1/databases/{database_id}/query'
+    
+    r = requests.post(url, headers={
+        'Authorization': f'Bearer {token}',
+        'Notion-Version': '2021-08-16'
+        })
+    result_dict = r.json()
+    return result_dict
+
+dados_coletados = get_dados_notion()
+
+df_evolucao = pd.DataFrame(columns=['dia','nota','pai','calorias','tempo_jogado'])
+lista_dias = []
+for item in range(0,len(dados_coletados)-1):
+    dia = dados_coletados['results'][item]['properties']['dia']['date']['start']
+    nota = dados_coletados['results'][item]['properties']['nota']['number']
+    pai = dados_coletados['results'][item]['properties']['pai']['number']
+    calorias = dados_coletados['results'][item]['properties']['calorias']['number']
+    tempo_jogado = dados_coletados['results'][item]['properties']['tempo jogado (min)']['number']
+    lista_dias.append({'dia':dia, 
+                       'nota':nota, 
+                       'pai':pai, 
+                       'calorias':calorias, 
+                       'tempo_jogado':tempo_jogado})
+    
+df_evolucao = df_evolucao.append(lista_dias, ignore_index=False)
+print(df_evolucao)
