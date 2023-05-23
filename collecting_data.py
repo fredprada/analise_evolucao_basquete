0f8b4b410e70785147a1474c4fe326c90e3ab2af
Frederico Loiola Prada <74056437+fredprada@users.noreply.github.com>
2023-05-22 23:07:43 -0300

    Deleting collecting_data.py

--- collecting_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def treated_data():
-    # reading the data
-    global df_raw
-    arquivo_base = "dataset_compras.xlsx - Sheet1.csv"
-    df_raw = pd.read_csv(arquivo_base)
-
-    return df_raw
