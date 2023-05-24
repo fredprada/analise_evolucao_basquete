e262364b19e6598856f01bbda0a6a8134bc91906
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-23 22:35:06 -0300

    debugando código streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-from evolucao_basquete import get_dados_notion, treated_data
+# from evolucao_basquete import get_dados_notion, treated_data
 import streamlit as st
 # import numpy as np
 # import scipy
