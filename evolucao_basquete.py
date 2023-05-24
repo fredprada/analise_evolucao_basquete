86c617e43f4c0ba6efec112618d1a635db7755d7
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-24 00:58:58 -0300

    adicionando novas colunas do notion

--- evolucao_basquete.py
+++ evolucao_basquete.py
@@ -16,19 +16,21 @@ def get_dados_notion(token, database_id):
 
 def treated_data(dados_coletados):
     global df_evolucao
-    # df_evolucao = pd.DataFrame(columns=['dia','nota','pai','calorias','tempo_jogado'])
     lista_dias = []
     for item in range(0, len(dados_coletados['results'])):
         dia = dados_coletados['results'][item]['properties']['dia']['date']['start']
-        nota = dados_coletados['results'][item]['properties']['nota']['number']
+        nota = dados_coletados['results'][item]['properties']['nota da atuação']['number']
         pai = dados_coletados['results'][item]['properties']['pai']['number']
         calorias = dados_coletados['results'][item]['properties']['calorias']['number']
         tempo_jogado = dados_coletados['results'][item]['properties']['tempo jogado (min)']['number']
+        animo_pra_jogar = dados_coletados['results'][item]['properties']['animo pra jogar']['number']
+        sentimento_do_dia = dados_coletados['results'][item]['properties']['sentimento no dia']['number']
         lista_dias.append({'dia':dia, 
                            'nota':nota, 
                            'pai':pai, 
                            'calorias':calorias, 
-                           'tempo_jogado':tempo_jogado})
-    # df_evolucao = df_evolucao.append(lista_dias)
+                           'tempo_jogado':tempo_jogado,
+                           'animo_pra_jogar':animo_pra_jogar,
+                           'sentimento_do_dia':sentimento_do_dia})
     df_evolucao = pd.DataFrame(lista_dias)
     return df_evolucao
