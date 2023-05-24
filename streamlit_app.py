913b3a16884afcb35be8d7f6e1dd3280a081416e
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-23 22:33:35 -0300

    debugando código streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,29 +19,32 @@ database_id = os.environ.get('NOTION_DATABASE_ID')
 dados_coletados = get_dados_notion(token, database_id)
 df_treated_data = treated_data(dados_coletados)
 
-# introducing context for the analysis
-st.title("Análise de evolução de Basquete")
-st.header("Essa análise é focada em entender se há uma tendência entre as variáveis coletadas depois de um jogo de basquete.")
-st.write("""Gosto bastante de jogar basquete, e quero entender se há essa correlação entre as variáveis, e se estou tendo algum avanço na nota que dou para meu jogo.""")
+st.write(f"Token: {token}")
+st.write(f"Database ID: {database_id}")
 
-# showing descriptive statistics metrics and some insights
-col1, col2 = st.columns([2,3])
-col1.write(df_treated_data.describe())
+# # introducing context for the analysis
+# st.title("Análise de evolução de Basquete")
+# st.header("Essa análise é focada em entender se há uma tendência entre as variáveis coletadas depois de um jogo de basquete.")
+# st.write("""Gosto bastante de jogar basquete, e quero entender se há essa correlação entre as variáveis, e se estou tendo algum avanço na nota que dou para meu jogo.""")
 
-col2.subheader("Boxplot dos dias que joguei")
-fig = px.line(df_treated_data.sort_values(by='dia', ascending=True), 
-                 y = 'pai', 
-                 x = 'dia',
-                 markers=True)
-fig.update_layout(
-    autosize=False,
-    width=600,
-    height=300,
-    margin=dict(
-        l=30,
-        r=30,
-        b=60,
-        t=30,
-        pad=4
-    ))
-col2.plotly_chart(fig, theme=None)
+# # showing descriptive statistics metrics and some insights
+# col1, col2 = st.columns([2,3])
+# col1.write(df_treated_data.describe())
+
+# col2.subheader("Boxplot dos dias que joguei")
+# fig = px.line(df_treated_data.sort_values(by='dia', ascending=True), 
+#                  y = 'pai', 
+#                  x = 'dia',
+#                  markers=True)
+# fig.update_layout(
+#     autosize=False,
+#     width=600,
+#     height=300,
+#     margin=dict(
+#         l=30,
+#         r=30,
+#         b=60,
+#         t=30,
+#         pad=4
+#     ))
+# col2.plotly_chart(fig, theme=None)
