5c1a62b17b841ac26026be7d0d47c6bad6e8fbf3
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-28 10:08:04 -0300

    Nova versão conectando no mongodb

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,62 @@
+###################################################################
 import streamlit as st
 import plotly.express as px
 import os
-from coleta_de_dados import get_dados_notion, first_treatment
-from tratamento_de_dados import transform_data
 
-# definindo configurações iniciais da página
+###################################################################
+# Defining page properties
 st.set_page_config(
     layout="wide",  
     page_title="🏀 Análise de performance")
+
+###################################################################
+# Forms to collect latest game information
+st.title('Conta aqui como foi seu último jogo:')
+
+feelings_list = ['Radiante',
+                 'Feliz',
+                 'Entusiasmado',
+                 'Esperançoso',
+                 'Animado',
+                 'Meh',
+                 'Cansado',
+                 'Desanimado',
+                 'Triste',
+                 'Frustrado']
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    date_of_the_game = st.date_input('Data do jogo')
+    time_played = st.time_input('Tempo que jogou (min)')
+
+    pai = st.number_input('Quantos PAI você ganhou?', 
+                            min_value = 0)
+    played_alone = st.radio('Você jogou sozinho?',
+                            ['sim', 'não'])
+with col2:
+    time_of_the_game = st.time_input('Hora do jogo')
+    mood_before_playing = st.number_input('Qual era seu ânimo para jogar?', 
+                                        min_value = 0, 
+                                        max_value = 10)
+    rating = st.number_input('Qual nota você dá pro seu jogo?',
+                            min_value = 0, 
+                            max_value = 10)
+    listened_to_music = st.radio('Você ouviu música?',
+                             ['sim', 'não'])
+
+with col3:
+    rest_time = st.time_input('Tempo de descanso (min)')
+    feeling_before_game = st.selectbox('Como você tava se sentindo antes de jogar?',
+                                       (feelings_list))
+    calorias = st.number_input('Quantas calorias você perdeu?', 
+                                min_value = 0)
+
+with col1:
+    button_add_row = st.button('Adicionar')
+
+with col2:
+    button_remove_row = st.button('Remover última entrada')
 
 # credenciais para acessar API
 token = os.getenv('NOTION_BASQUETE_TOKEN')
@@ -49,3 +98,6 @@ col2.caption("Gráfico do índice 'PAI' dos dias que joguei.")
 col2.caption("PAI é o *personal activity intelligence*, da Amazfit, que dá uma nota ao exercício físico de acordo com certos critérios.")
 
 st.metric(label="Número de dias jogados", value=dias_jogados, delta=1)
+
+
+
