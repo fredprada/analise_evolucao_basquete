2a0e92b0acae4bf146edfba1176801dae0d7908a
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-28 22:49:48 -0300

    Tentando mover as funções pro código de insert to database

--- insert_to_database.py
+++ insert_to_database.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from streamlit_app import func_add_row
 
 def connect_to_mongodb(client):
     global collection
@@ -16,11 +15,10 @@ def insert_information(collection, list_to_insert):
     except:
         return print("Informações não foram inseridas corretamente")
 
-def call_database_insertion():
+def call_database_insertion(list_to_add):
     # client = os.getenv('CLIENT_TOKEN')
     client = "mongodb+srv://conexao-api:dmi4zj8EuJbExh9l@personal-cluster.gdixbl3.mongodb.net/?retryWrites=true&w=majority"
     collection = connect_to_mongodb(client)
-    list_to_add = func_add_row()
     # st.sidebar.text('Tentando adicionar ao banco via insert_information')
     insert_information(collection, list_to_add)
     # st.sidebar.text(insertion)
