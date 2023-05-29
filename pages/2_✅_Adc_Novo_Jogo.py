4a8357c699d13d0f6b2e3820d39034e0f3e67e27
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-29 00:48:20 -0300

    Adicionando páginas dentro da página central

--- pages/2_✅_Adc_Novo_Jogo.py
+++ pages/2_✅_Adc_Novo_Jogo.py
@@ -62,14 +62,15 @@ def func_add_row(date_of_the_game,time_played,pai,played_alone,time_of_the_game,
     global list_to_add
     list_to_add=[]
     dict_dia = {}
+    dict_dia['jogador'] = player
     dict_dia['dia'] = date_of_the_game.strftime("%Y-%m-%d")
     dict_dia['hora_do_jogo'] = time_of_the_game.strftime("%H:%M")
     dict_dia['tempo_de_descanso'] = str(rest_time)
     dict_dia['jogou_sozinho'] = str(played_alone)
     dict_dia['ouviu_musica'] = str(listened_to_music)
     dict_dia['nota'] = str(rating)
-    dict_dia['pai'] = str(pai) 
-    dict_dia['calorias'] = str(calorias) 
+    dict_dia['pai'] = str(pai)
+    dict_dia['calorias'] = str(calorias)
     dict_dia['tempo_jogado'] = str(time_played)
     dict_dia['animo_pra_jogar'] = str(enthusiasm_before_playing)
     dict_dia['sentimento_do_dia'] = str(feeling_before_game)
