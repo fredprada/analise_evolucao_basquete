48f5eb5be11b9c247412343ce9dde9ade620864d
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-28 21:22:50 -0300

    Corrigindo bugs

--- insert_to_database.py
+++ insert_to_database.py
@@ -1,12 +1,10 @@
 from pymongo import MongoClient
 
 def connect_to_mongodb(client):
-    global collection
     myclient = MongoClient(client)
     print("Connected successfully!!!")
     db = myclient.get_database('db_evolucao_basquete')
     collection = db.collection_evolucao_basquete
-    collection = connect_to_mongodb(client)
     return collection
 
 def insert_information(collection, list_to_insert):
