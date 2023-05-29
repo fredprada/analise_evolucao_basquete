d99efe57e97486550fedcbbe168b2eeddc47526f
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-28 22:24:10 -0300

    Adicionando on_click no botão add row

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def call_database_insertion():
     # client = os.getenv('CLIENT_TOKEN')
     client = "mongodb+srv://conexao-api:dmi4zj8EuJbExh9l@personal-cluster.gdixbl3.mongodb.net/?retryWrites=true&w=majority"
     collection = connect_to_mongodb(client)
-    list_to_add = func_add_row()
+    list_to_add = func_add_row(date_of_the_game,time_played,pai,played_alone,time_of_the_game,enthusiasm_before_playing,rating,listened_to_music,rest_time,feeling_before_game,calorias)
     insert_information(collection, list_to_add)
     print('Tentando adicionar ao banco via insert_information')
 
