96ae7f5f8d1cc8d0c39b385df1d70feb66050c4c
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-29 00:07:01 -0300

    Adicionando uma página central

--- Home.py
+++ Home.py
@@ -1,3 +1,17 @@
-Home.py
-    pages/
-        streamlit_app.py 
+import streamlit as st
+
+def new_game_forms():
+    import streamlit as st
+
+    st.write(
+        """
+        PÁGINA PARA ADC NOVO JOGO
+        """
+    )
+
+page_names_to_funcs = {
+    "new_game_forms Demo": new_game_forms
+}
+
+pages = st.sidebar.selectbox("Escolha uma página", page_names_to_funcs.keys())
+page_names_to_funcs[pages]()
