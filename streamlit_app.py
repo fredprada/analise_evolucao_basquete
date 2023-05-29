32980e639fee0de9952d8bacefa2ef5e9ed2d292
Frederico Loiola Prada <fredprada9@gmail.com>
2023-05-28 23:11:28 -0300

    Teste/debugando

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,17 +52,17 @@ def func_add_row(date_of_the_game,time_played,pai,played_alone,time_of_the_game,
     global list_to_add
     list_to_add=[]
     dict_dia = {}
-    dict_dia['dia'] = date_of_the_game
-    dict_dia['hora_do_jogo'] = time_of_the_game
-    dict_dia['tempo_de_descanso'] = rest_time
-    dict_dia['jogou_sozinho'] = played_alone
-    dict_dia['ouviu_musica'] = listened_to_music
-    dict_dia['nota'] = rating
-    dict_dia['pai'] = pai 
-    dict_dia['calorias'] = calorias 
-    dict_dia['tempo_jogado'] = time_played
-    dict_dia['animo_pra_jogar'] = enthusiasm_before_playing
-    dict_dia['sentimento_do_dia'] = feeling_before_game
+    dict_dia['dia'] = date_of_the_game.strftime("%Y-%m-%d")
+    dict_dia['hora_do_jogo'] = time_of_the_game.strftime("%H:%M")
+    dict_dia['tempo_de_descanso'] = str(rest_time)
+    dict_dia['jogou_sozinho'] = str(played_alone)
+    dict_dia['ouviu_musica'] = str(listened_to_music)
+    dict_dia['nota'] = str(rating)
+    dict_dia['pai'] = str(pai) 
+    dict_dia['calorias'] = str(calorias) 
+    dict_dia['tempo_jogado'] = str(time_played)
+    dict_dia['animo_pra_jogar'] = str(enthusiasm_before_playing)
+    dict_dia['sentimento_do_dia'] = str(feeling_before_game)
     list_to_add.append(dict_dia)
     return list_to_add
 
