dcc8f1fb01992bb87ade94189157be2f2fd41d32
fredprada <fredprada9@gmail.com>
2023-05-30 02:30:31 -0300

    Editando import

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # import plotly.express as px
-import transform_to_dataframe
+from adc_novo_jogo import transform_to_dataframe
 
 ###################################################################
 # Defining page properties and title, header and subheader
