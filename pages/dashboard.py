48bf93e4e56c16da9604e21deb494a2e7aacee22
fredprada <fredprada9@gmail.com>
2023-05-30 02:46:13 -0300

    adc functions.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,7 +8,9 @@ st.set_page_config(page_title = "📈 Dashboard")
 
 col1, col2 = st.columns(2)
 
+df_all_info = transform_to_dataframe()
+dias_jogados = len(df_all_info['dia'].value_counts())
+
 with col1:
     st.header('Sobre o seu último jogo')
-    dias_jogados = transform_to_dataframe()
     st.metric(label="Número de dias jogados", value=dias_jogados, delta=1)
