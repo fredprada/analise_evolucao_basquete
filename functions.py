80f79e43f6b579050789f440bd01aa24caea4a64
Frederico Loiola Prada <fredprada9@gmail.com>
2023-06-23 02:54:42 -0300

    Corrigindo functions e dashboard

--- functions.py
+++ functions.py
@@ -98,6 +98,6 @@ def main_metrics(dataframe, player):
     
     df_games_by_week = (pd.DataFrame(specific_dataframe['numero_da_semana'].value_counts().sort_index(ascending = False))).reset_index()
     df_games_by_week = df_games_by_week.rename(columns={'numero_da_semana':'qtd_jogos', 'index':'numero_da_semana'})
-    jogos_essa_semana = df_games_by_week[df_games_by_week['numero_da_semana'] == current_week]['qtd_jogos']
+    jogos_essa_semana = df_games_by_week[df_games_by_week['numero_da_semana'] == current_week]['qtd_jogos'][0]
 
     return jogos_essa_semana
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # import plotly.express as px
-from functions import transform_to_dataframe, main_metrics
+from functions import transform_to_dataframe, main_metrics, data_transformation
 
 ###################################################################
 # Defining page properties and title, header and subheader
@@ -13,6 +13,7 @@ players_list = ['Bia',
 
 df_all_info = transform_to_dataframe()
 player = st.selectbox('Jogador(a):', (players_list))
+data_transformation(df_all_info)
 
 main_metrics_list = main_metrics(df_all_info, player)
 dias_jogados = len(df_all_info['dia'].value_counts())
