66e80cfd587b58703e8ae16bf827f31d0f766900
Frederico Loiola Prada <fredprada9@gmail.com>
2023-06-23 17:41:17 -0300

    Corrigindo functions e dashboard

--- functions.py
+++ functions.py
@@ -98,6 +98,10 @@ def main_metrics(dataframe, player):
     
     df_games_by_week = (pd.DataFrame(specific_dataframe['numero_da_semana'].value_counts().sort_index(ascending = False))).reset_index()
     df_games_by_week = df_games_by_week.rename(columns={'numero_da_semana':'qtd_jogos', 'index':'numero_da_semana'})
-    # jogos_essa_semana = df_games_by_week[df_games_by_week['numero_da_semana'] == current_week]['qtd_jogos'][0]
-    jogos_essa_semana = df_games_by_week.copy()
+    jogos_essa_semana = df_games_by_week.query(f'numero_da_semana == {current_week}')['qtd_jogos'][0]
+    if jogos_essa_semana > 0:
+        jogos_essa_semana
+    else:
+        jogos_essa_semana = 0
+
     return jogos_essa_semana
