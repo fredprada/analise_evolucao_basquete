916b0f0ac3e6ffc321ebcf7999ecd6a7f82c66ff
Frederico Loiola Prada <fredprada9@gmail.com>
2023-06-23 18:13:11 -0300

    Corrigindo functions e dashboard

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,26 +1,12 @@
 import streamlit as st
-# import plotly.express as px
+import plotly.express as px
 from functions import transform_to_dataframe, main_metrics, data_transformation
 
 ###################################################################
 # Defining page properties and title, header and subheader
 st.set_page_config(page_title = "📈 Dashboard")
-page_bg_img = '''
-    <style>
-    body {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: cover;
-    }
-    </style>
-'''
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
-players_list = ['Bia',
-                'Fred']
-player = st.selectbox('Jogador(a):', (players_list))
-
-col1, col2 = st.columns(2)
+player = st.radio('Jogador(a):', ['Bia','Fred'])
 
 df_all_info = transform_to_dataframe()
 data_transformation(df_all_info)
@@ -31,6 +17,7 @@ jogos_essa_semana = main_metrics(df_all_info, player)
 
 st.header(f'Oi {player}!')
 st.subheader('Dá uma olhadinha aqui nas suas estatísticas')
-st.metric(label="Número de dias jogados", value=dias_jogados, delta=1)
 
-st.metric(label='jogos essa semana: ', value = jogos_essa_semana)
+col1, col2 = st.columns(2)
+col1.metric(label="Número de dias jogados", value=dias_jogados, delta=1)
+col2.metric(label='jogos essa semana: ', value = jogos_essa_semana, delta=1)
